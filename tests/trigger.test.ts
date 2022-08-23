cc6a11bcd3207e69f2f998bcdfef8d8b6276747a
Hashemi Rafsan <rafsanhashemi@gmail.com>
2022-08-23 17:10:34 +0600

    inital commit

---
+++ tests/trigger.test.ts
@@ -0,0 +1,17 @@
+import { DispatcherEngine, DispatchableEvent, DispatchableListener } from '../src/index';
+
+test('Init Dispatchable Event', () => {
+    // const dispatcher = new DispatcherEngine();
+    // const event = new DispatchableEvent('add-member');
+    // const firstListener = new DispatchableListener(() => {
+    //     return;
+    // });
+    // const secondListener = new DispatchableListener(() => {
+    //     return;
+    // }); 
+
+    // event.setListeners([firstListener, secondListener]);
+    // dispatcher.setEvents([event]).register();
+
+    // dispatcher.fire('add-member');
+})
